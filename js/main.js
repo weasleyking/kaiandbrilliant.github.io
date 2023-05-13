978f68afd1d6a8b7a962b68a737275b23efbcbbe
Kaifeng Wu <31644381+weasleyking@users.noreply.github.com>
2023-05-13 11:47:44 -0400

    more robust algorithm for scrolling

--- js/main.js
+++ js/main.js
@@ -10,6 +10,31 @@ function disableScroll() {
 
 function scrollToSection(direction) {
   let sections = Array.from(document.getElementsByClassName('container'));
+
+  let currentSectionIndex = sections.reduce((highestIndex, currentSection, currentIndex) => {
+    let viewportOffset = currentSection.getBoundingClientRect();
+    let topInView = viewportOffset.top >= 0 && viewportOffset.top < window.innerHeight;
+    let bottomInView = viewportOffset.bottom > 0 && viewportOffset.bottom <= window.innerHeight;
+    let inView = (bottomInView && topInView) ? viewportOffset.height :
+                 topInView ? viewportOffset.bottom :
+                 bottomInView ? window.innerHeight - viewportOffset.top : 0;
+
+    return inView > highestIndex.inView ? {inView: inView, index: currentIndex} : highestIndex;
+  }, {inView: 0, index: 0}).index;
+
+  let targetSectionIndex = currentSectionIndex + (direction === 'up' ? -1 : 1);
+
+  if (targetSectionIndex >= 0 && targetSectionIndex < sections.length) {
+    window.scrollTo({
+      top: sections[targetSectionIndex].offsetTop,
+      behavior: 'smooth',
+    });
+  }
+}
+
+/*
+function scrollToSection(direction) {
+  let sections = Array.from(document.getElementsByClassName('container'));
   let currentSectionIndex = sections.findIndex((section) => {
     let midpoint = section.offsetTop + section.offsetHeight / 2;
     return window.scrollY >= section.offsetTop && window.scrollY < midpoint;
@@ -30,6 +55,7 @@ function scrollToSection(direction) {
     });
   }
 }
+*/
 
 
 disableScroll();
